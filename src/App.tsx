a9f8b162eb34b3037c70e8914c852bc9e4498e72
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-05-23 15:42:36 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Inicio</h1>}></Route>
+          <Route path="/login" element={<h1>Entrar</h1>}></Route>
+          <Route path="/sigin" element={<h1>Cadastro de conta</h1>}></Route>
+          <Route path="/stock" element={<h1>Estoque</h1>}></Route>
+          <Route
+            path="/createproduct"
+            element={<h1>Cadastro de produto</h1>}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
