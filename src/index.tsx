c888bd6149091def71e8db387888f234961df525
Vinicius Aguiar <viniciusaguiar181@gmail.com>
2022-05-24 11:41:48 -0300

    ad default theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import GlobalStyle from "./style/style.global";
+import "./style/style.global.css";
+import { ThemeProvider } from "@mui/system";
+import theme from "./theme/default";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
